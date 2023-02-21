48f6eb08ca65ec07759363e2018287b649ce3025
SIWON990327 <pos06058@naver.com>
2023-02-21 21:20:04 +0900

    Router

--- nodejs/learn-express/app.js
+++ nodejs/learn-express/app.js
@@ -7,6 +7,10 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config();
+const indexRouter = require('./routes');
+const userRouter = require('./routes/user');
+
+dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
@@ -27,6 +31,14 @@ app.use(session({
     },
     name : 'session-cookie',
 }));
+
+app.use('/', indexRouter);
+app.use('/user',userRouter);
+
+app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+});
+
 
 const multer = require('multer');
 const fs = require('fs');
