1d58548760fde91af03b1dfbf9d8bdd7d08d8f59
SIWON990327 <pos06058@naver.com>
2023-02-21 22:14:39 +0900

    nunjucks

--- nodejs/learn-express/app.js
+++ nodejs/learn-express/app.js
@@ -2,98 +2,54 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const path = require('path');
+const nunjucks = require('nunjucks');
 
 dotenv.config();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
 
-dotenv.config();
 const app = express();
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
 
 app.use(morgan('dev'));
-
-app.use(morgan('dev')); //morgan 미들웨어
-app.use('/', express.static(path.join(__dirname, 'public'))); // '/'=요청경로 , static(실제경로) 
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
-app.use(bodyParser.raw());
-app.use(bodyParser.text());
-app.use(cookieParser(process.env.COOKIE_SECRET)); // cookieParesr(비밀키)
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave : false,
-    saveUninitialized : false,
-    secret : process.env.COOKIE_SECRET,
-    cookie : {
-        httpOnly : true,
-        secure : false,
-    },
-    name : 'session-cookie',
+  resave: false,
+  saveUninitialized: false,
+  secret: process.env.COOKIE_SECRET,
+  cookie: {
+    httpOnly: true,
+    secure: false,
+  },
+  name: 'session-cookie',
 }));
 
 app.use('/', indexRouter);
-app.use('/user',userRouter);
+app.use('/user', userRouter);
 
 app.use((req, res, next) => {
-    res.status(404).send('Not Found');
-});
-
-
-const multer = require('multer');
-const fs = require('fs');
-
-try {
-    fs.readdirSync('uploads');
-} catch (error) {
-    console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-    fs.mkdirSync('uploads');
-}
-const upload = multer({
-    storage : multer.diskStorage({
-        destination(req, file, done) {
-            done(null, 'uploads/');
-        },
-        filename(req, file, done) {
-            const ext = path.extname(file.originalname);
-            done(null, path.basename(file.originalname, ext) + Data.now() + ext);
-        },
-    }),
-    limits : { fileSize : 5 * 1024 * 1024},
-});
-app.get('/upload', (req, res) => {
-    res.sendFile(path.join(__dirname, 'multipart.html'));
-});
-app.post('/upload',
-    upload.fields([{ name : 'image1' }, { name : 'image2' }]),
-    (req, res) =>{
-        console.log(req.files, req.body);
-        res.end('ok');
-    },
-);
-
-app.use((req, res, next) => {
-    console.log('모든 요청에 다 실행됩니다.');
-    next();
-})
-
-app.get('/', (req, res, next) => {
-   console.log ('GET / 요청에서만 실행됩니다.');
-   next();
-}, (req, res) => {
-    throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기 중')
+  console.log(app.get('port'), '번 포트에서 대기 중');
 });
